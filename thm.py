30e646fbe414d5eab1a50ec63c6169a0226a3500
wasfyelbaz <wasfylbaz@gmail.com>
2021-09-30 10:00:33 +0200

    Added docstrings to the functions

--- thm.py
+++ thm.py
@@ -24,15 +24,19 @@ class THMProfile:
             number_of_current_shown_rooms = self.get_number_of_current_shown_rooms()
 
     def get_number_of_completed_rooms(self, page_source):
+        """:returns the number of completed rooms"""
         return int(BeautifulSoup(page_source, features='html.parser').find('div', {'id': 'rooms-completed'}).text)
 
     def get_number_of_current_shown_rooms(self):
+        """:returns the number of currently shown rooms"""
         soup = BeautifulSoup(self.driver.page_source, features='html.parser')
         return len(soup.find_all('div', {'class': 'room-card-design-title'}))
 
     def show_more_rooms(self):
+        """get more rooms with JS function"""
         self.driver.execute_script('completedRoomsAdd()')
 
     def get_all_completed_rooms(self):
+        """:returns the name of all completed rooms"""
         soup = BeautifulSoup(self.driver.page_source, features='html.parser')
         return [room_name.text for room_name in soup.find_all('div', {'class': 'room-card-design-title'})]
