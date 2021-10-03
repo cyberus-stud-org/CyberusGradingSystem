d65ad0ac123d95674a10e83e374fbae16db3d722
Wasfy Elbaz <wasfylbaz@gmail.com>
2021-10-03 16:51:43 +0200

    Added queue sqlite db file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 THM_PROFILE_URL = 'https://tryhackme.com/p/'
 CYBERTALENTS_PROFILE_URL = 'https://cybertalents.com/members/'
 
-DB_FILE_NAME = 'testdb.db'
+DB_FILE_NAME = 'queue.db'
 TABLE_NAME = 'thm_grading_queue'
 USERNAME_COL_NAME = 'username'
 CHALLENGE_COL_NAME = 'challenge_name'
