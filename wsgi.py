ffc370ab6d22d7717a54980be6a8307dc9c95eb2
wasfyelbaz <wasfylbaz@gmail.com>
2022-03-03 18:35:23 +0200

    Added authentication system with JWT tokens

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,22 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+
+import datetime
+import os
 
 from THMGradingSystem import THMGradingSystem
 import config
 
 THMGradingSystem = THMGradingSystem()
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(minutes=int(os.getenv("JWT_EXPIRES_AFTER_HOURS"))*60)
+app.config["JWT_IDENTITY_CLAIM"] = "domain"
+
+jwt = JWTManager(app)
 
 
 def json_response(msg: str, state: int):
@@ -25,7 +36,6 @@ def json_response(msg: str, state: int):
 
 def verify_json_keys(json_data, keys_to_verify):
     """verifies that a list of keys exist in the JSON data and :returns a list of missing keys"""
-    # ["username", "password"]
     missing_kays = []
 
     # verify JSON data
@@ -43,7 +53,33 @@ def index():
     return ""
 
 
+@app.route("/api/authenticate", methods=["GET", "POST"])
+def authenticate():
+    """handles the user authentication process"""
+    if request.method == "GET":
+        return json_response("No auth credentials provided", 0)
+    # get JSON data from request
+    try:
+        json_data = request.get_json()
+    except Exception:
+        return jsonify({"msg": "Invalid JSON format"}), 400
+
+    # verify JSON data
+    missing_keys = verify_json_keys(json_data, ["domain", "key"])
+    if len(missing_keys) > 0:
+        return json_response("Missing " + ", ".join(missing_keys), 0), 400
+
+    # return access token if authenticated successfully
+    if json_data["domain"] == os.getenv("ALLOWED_DOMAIN") \
+            and json_data["key"] == str(os.getenv("API_KEY")):
+        access_token = create_access_token(identity=json_data["domain"])
+        return jsonify(access_token=access_token)
+
+    return jsonify({"msg": "Invalid username or password"}), 401
+
+
 @app.route("/api/CheckTHM", methods=["GET", "POST"])
+@jwt_required()
 def check_thm():
     """check if user solved a specific room on TryHackMe"""
     # get JSON data from request
@@ -71,6 +107,7 @@ def check_thm():
 
 
 @app.route("/api/RestartTHM")
+@jwt_required()
 def restart_thm_system():
     """restart the THM grading system"""
     try:
@@ -82,6 +119,7 @@ def restart_thm_system():
 
 
 @app.route("/api/CheckCyberTalents", methods=["GET", "POST"])
+@jwt_required()
 def check_cybertalents():
     """check if user solved a specific challenge on CyberTalents"""
     # get JSON data from request
